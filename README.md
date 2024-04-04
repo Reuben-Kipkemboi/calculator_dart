f72afc60746bc7615e3efb4a2126a59636348b70
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-04-04 09:21:28 +0300

    ft:format-string - format string output

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# calculator_dart
+## calculator_dart
--- calc.dart
+++ calc.dart
@@ -2,7 +2,7 @@ import "dart:io";
 import "dart:math";
 
 double userInput(){
-  print("Could you Please enter your Number:");
+  print("Could you Please enter a Number of choice:");
   double userNumber = double.parse(stdin.readLineSync()!);
   return userNumber;
 }
@@ -10,7 +10,12 @@ double userInput(){
 // Prompt user to give the operand/operation they want to undertake
 
 String operandInput(){
-  print("Could you Please enter your Operation e.g '+','-','*','/','%','',''");
+  print("Could you Please enter your Operation e.g '+','-','*','/','%'");
+  print("+ for Addition");
+  print("- for Subtraction");
+  print("* for Multiplication");
+  print("/ for Division");
+  print("% for Modulus division");
   String userOperation = stdin.readLineSync()!;
   return userOperation;
 }
@@ -25,20 +30,22 @@ void main(){
 
   if (operationOfChoice == "+"){
     valueOfOperation = firstNumber + secondNumber;
-    print("${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
+    // print("Totals = ${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
+    print("Totals = \x1B[1m$firstNumber \x1B[33m$operationOfChoice \x1B[1m$secondNumber = \x1B[31m$valueOfOperation");
   }else if(operationOfChoice == "-"){
     valueOfOperation =firstNumber - secondNumber;
-    print("${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
+    print("Totals = ${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
   }else if(operationOfChoice == "/"){
     valueOfOperation =firstNumber / secondNumber;
-    print("${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
+    print("Totals =${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
   }else if(operationOfChoice == "*"){
     valueOfOperation =firstNumber * secondNumber;
-    print("${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
+    print("Totals =${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
     
   }else if(operationOfChoice == "%"){
     valueOfOperation =firstNumber % secondNumber;
-    print("${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
+    // print("Totals = ${firstNumber} ${operationOfChoice} ${secondNumber} = ${valueOfOperation}");
+    print("Totals = \x1B[1m$firstNumber \x1B[33m$operationOfChoice \x1B[1m$secondNumber = \x1B[31m$valueOfOperation");
   }else(){
     print("You've entered the wrong operation");
 };
